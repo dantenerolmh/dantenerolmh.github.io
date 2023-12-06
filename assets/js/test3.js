14ffd0aa729742a6623408bc2ddd5e8b17ae220f
Taoistboyz <dantenerolmh@live.com>
2023-12-06 14:30:42 +0800

    test "test3.js"

---
+++ assets/js/test3.js
@@ -0,0 +1,17 @@
+
+function setup() {
+  var canvas = createCanvas(windowWidth, windowHeight);   //创建canvas，参数是窗口的宽高
+  canvas.style('display', 'block');
+  canvas.parent('page-top');
+
+
+}
+
+function draw() {
+  if (mouseIsPressed) {
+    fill(0);
+  } else {
+    fill(255);
+  }
+  ellipse(mouseX, mouseY, 80, 80);
+}
--- assets/js/testMouseWheel.js
+++ assets/js/testMouseWheel.js
@@ -1,13 +1,15 @@
+//用鼠标滚轮上下左右滚动触发事件的示例
+
 function setup() {
     // Create a canvas element and
     // assign it to cnv.
-    let cnv = createCanvas(800, 800);
+    let cvs = createCanvas(800, 800);
   
     background(200);
   
     // Call changeBackground() when the
     // mouse wheel moves.
-    cnv.mouseWheel(changeBackground);
+    cvs.mouseWheel(changeBackground);
   
     describe('A gray square. When the mouse wheel scrolls over the square, it changes color and displays shapes.');
   }
@@ -15,18 +17,18 @@ function setup() {
   function changeBackground(event) {
     // Change the background color
     // based on deltaY.
-    if (event.deltaY > 0) {
+    if (event.deltaY > 0) { //滚轮往↓滚是+
       background('deeppink');
-    } else if (event.deltaY < 0) {
+    } else if (event.deltaY < 0) {  //滚轮往↑滚是-
       background('cornflowerblue');
     } else {
       background(200);
     }
   
     // Draw a shape based on deltaX.
-    if (event.deltaX > 0) {
+    if (event.deltaX > 0) { //滚轮往→滚是+
       circle(50, 50, 20);
-    } else if (event.deltaX < 0) {
+    } else if (event.deltaX < 0) {  //滚轮往←滚是-
       square(40, 40, 20);
     }
   }
